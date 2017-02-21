3409d0892e303ee561fd31560a9c60f5f2357e37
Pedro J. Martínez <pedrojmrt@gmail.com>
2017-02-21 22:25:11 +0100

    Routes: added /welcome

--- app/public/routes/index.jsx
+++ app/public/routes/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {MessageApp} from '../js/components/message';
+import { MessageApp } from '../js/components/message';
+import { Router, Route, hashHistory, Redirect } from 'react-router';
 
-ReactDOM.render(<MessageApp />, rootNode);
+// We're using routes from react-routes on client side
+// instead of the node ones.
+
+ReactDOM.render(
+		<Router history={hashHistory}>
+			<Redirect from="/" to="/welcome" />
+			<Route path="/welcome" component={MessageApp} />
+		</Router>
+	, rootNode);
+
+// ReactDOM.render(<MessageApp />, rootNode);
